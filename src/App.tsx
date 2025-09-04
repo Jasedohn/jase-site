d6809f38399807fd4f764494742bde2bd619da51
Jase Dohn <jasedohn@gmail.com>
2025-09-03 22:00:52 -0500

    Remove Projects.html file and update package.json and package-lock.json to include react-router-dom dependency

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,43 @@
 import { useState } from 'react'
 import botLogo from './assets/download.png'
 import './App.css'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Projects from './Projects';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <>
-      <div>
-        <a href="Projects.html" target="_blank">
-          <img src={botLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Jase Dohn</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Hello what are you doing here? This is a personal website for Jase Dohn.
-        </p>
-      </div>
-      <p className="read-the-docs">
-        In development...
-      </p>
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <div>
+                <Link to="/projects">
+                  <img src={botLogo} className="logo react" alt="React logo" />
+                </Link>
+              </div>
+              <h1>Jase Dohn</h1>
+              <div className="card">
+                <button onClick={() => setCount((count) => count + 1)}>
+                  count is {count}
+                </button>
+                <p>
+                  Hello what are you doing here? This is a personal website for Jase Dohn.
+                </p>
+              </div>
+              <p className="read-the-docs">
+                In development...
+              </p>
+            </>
+          }
+        />
+        <Route path="/projects" element={<Projects />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App
