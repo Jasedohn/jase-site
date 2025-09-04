0460988b3634b987da096b44d20007080e63a5ff
Jase Dohn <jasedohn@gmail.com>
2025-09-03 21:33:47 -0500

    Add download image asset

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import botLogo from './assets/download.png'
 import viteLogo from '/vite.svg'
 import './App.css'
 
@@ -13,7 +13,7 @@ function App() {
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img src={botLogo} className="logo react" alt="React logo" />
         </a>
       </div>
       <h1>Jase Dohn</h1>
