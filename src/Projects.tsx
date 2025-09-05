5ddd0e008da54ef06c3462eec725e8c57e602d64
Jase Dohn <jasedohn@gmail.com>
2025-09-04 22:11:06 -0500

    Deleting router from projects - not needed - JD

--- src/Projects.tsx
+++ src/Projects.tsx
@@ -1,29 +1,20 @@
+
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import homeLogo from './assets/25694.png';
 
 const Projects: React.FC = () => {
   return (
-    <Router>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <>
-            <div>
-              <Link to ="/">
-                <img src={homeLogo} className="logo react" alt="Home logo" />
-              </Link>
-            </div>
-              <div>
-                <h2>Projects</h2>
-              </div>
-            </>
-          }
-        />
-        <Route path="/projects" element={<Projects />} />
-      </Routes>
-    </Router>
+    <>
+      <div>
+        <Link to="/">
+          <img src={homeLogo} className="logo react" alt="Home logo" />
+        </Link>
+      </div>
+      <div>
+        <h2>Projects</h2>
+      </div>
+    </>
   );
 };
 
