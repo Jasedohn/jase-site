8e45d34b5df3a8730937553b4194f5d648463eee
Jase Dohn <jasedohn@gmail.com>
2025-09-07 16:04:47 -0500

    Message

--- src/Projects.tsx
+++ src/Projects.tsx
@@ -1,22 +1,35 @@
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import homeLogo from './assets/25694.png';
 
 const Projects: React.FC = () => {
+  const [pokemonNames, setPokemonNames] = useState<string[]>([]);
+
+  useEffect(() => {
+    fetch('https://pokeapi.co/api/v2/pokemon?limit=10')
+      .then(res => res.json())
+      .then(data => setPokemonNames(data.results.map((p: { name: string }) => p.name)));
+  }, []);
+
   return (
     <>
       <div>
-        <Link to="/">
+        <Link to="/"> 
           <img src={homeLogo} className="logo react" alt="Home logo" />
         </Link>
       </div>
       <div>
-        <h2>Discord Bot Project</h2>
+        <h2>Pokemon Project</h2>
       </div>
       <div>
-        <p>This Project is for adding a discord bot to my server - going to look into AI chat bot integration</p>
+        <p>This is going to give a list of pokemon that is searchable</p>
       </div>
+      <ul>
+        <li>Fetch data from the PokeAPI</li>
+        <li>Display a list of Pokemon names</li>
+        {pokemonNames.map((name: string) => <li key={name}>{name}</li>)}
+      </ul>
     </>
   );
 };
